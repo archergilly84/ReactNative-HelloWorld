02c3e202748748386435262d066c49d5b8a26fa0
David Gilligan <david@virticall.co.uk>
2020-02-19 15:11:32 +0000

    Init

---
+++ app/sections/Header.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {StyleSheet, Text, View}  from 'react-native';
+
+export class Header extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {isLoggedIn: false};
+    }
+
+    toggleUser = () => {
+        this.setState(previousState => {
+            return { isLoggedIn : !previousState.isLoggedIn };
+        });
+    }
+
+    render() {
+        let display = this.state.isLoggedIn ? 'Sample User' : this.props.message;
+        return (
+            <View style={styles.headStyle}>
+                <Text
+                    style={styles.headText} 
+                    onPress={this.toggleUser}>{display}
+                </Text>
+            </View>
+        );
+    }
+    
+} 
+
+const styles = StyleSheet.create({
+    headText: {
+        textAlign: 'right',
+        color: '#ffffff',
+        fontSize: 20
+    },
+    headStyle: {
+        paddingTop: 50,
+        paddingRight: 10,
+        backgroundColor: '#35605A',
+        flex : 1
+    }
+});
